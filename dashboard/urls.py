961cf78255f1f7c17910a044eb11c24f472da52e
franciscr7 <franciscr567@gmail.com>
2026-07-07 21:31:39 +0530

    Data preview added

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('upload/', views.upload_file, name='upload'),
+    path('preview/', views.data_preview, name='preview')
 ]
